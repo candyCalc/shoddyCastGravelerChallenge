a72144fd72f0c7e6e9ffcbd0ce0286b86b502118
crimew <109626643+candyCalc@users.noreply.github.com>
2024-08-21 03:26:20 -0700

    small variable names go brrrr

--- graveler.py
+++ graveler.py
@@ -1,8 +1,8 @@
 from os import urandom
 
-def roll():
-    bitset = int.from_bytes(urandom(59), 'big')
-    bit_slicer = 3450873173395281893717377931138512726225554486085193277581262111899647
-    return (bitset & bit_slicer & bitset >> 241).bit_count()
+def r():
+    x = int.from_bytes(urandom(59), 'big')
+    y = 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    return (x & y & x >> 241).bit_count()
 
-print(max([roll() for _ in range(1000000000)]))
+print(max([r() for _ in range(1000000000)]))
